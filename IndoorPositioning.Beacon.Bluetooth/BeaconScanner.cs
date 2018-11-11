d2b1468f08a6f1616010589d3a5317ab4e671390
taneresme <tnr.esme@gmail.com>
2018-11-11 20:25:43 +0300

    project organized accoring to UAP requirements

--- IndoorPositioning.Beacon.Bluetooth/BeaconScanner.cs
+++ IndoorPositioning.Beacon.Bluetooth/BeaconScanner.cs
@@ -1,14 +1,23 @@
 using IndoorPositioning.Beacon.Core;
+using Windows.Devices.Bluetooth.Advertisement;
 
 namespace IndoorPositioning.Beacon.Bluetooth
 {
     public class BeaconScanner : IBeaconScanner
     {
         private IBeacon _beacon;
+        private BluetoothLEAdvertisementWatcher _watcher;
 
         public BeaconScanner()
         {
             _beacon = new Beacon();
+            _watcher = new BluetoothLEAdvertisementWatcher();
+            _watcher.Received += _watcher_Received;
+        }
+
+        private void _watcher_Received(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
